8db48ff8b93d00f22ec838526291dbae53066a3c
Jordan Butterfield <jmcbutter@gmail.com>
2022-11-01 10:33:52 -0500

    Add Chakra UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider, Box } from '@chakra-ui/react'
+import theme from './theme';
+import './theme/styles.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Box bg='slightly-desaturated-cyan'>
+        <div>This is a test</div>
+      </Box>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ChakraProvider>
   );
 }
 
---
+++ src/theme/index.js
@@ -0,0 +1,18 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `Ibarra Real Nova, serif`,
+    body: `Public Sans, sans`,
+  },
+  colors: {
+    'bright-red': "#F43030",
+    'light-gray': "#EAEAEB",
+    'very-light-gray': "#FAFAFA",
+    'grayish-dark-blue': "#33323D",
+    'dark-blue': "#203A4C",
+    'slightly-desaturated-cyan': "#5FB4A2",
+  },
+});
+
+export default theme;
