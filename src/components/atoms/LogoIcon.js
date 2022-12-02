14a138be185e1427a9a762e535e19cfe6257c9ee
Jordan Butterfield <jmcbutter@gmail.com>
2022-12-02 08:27:26 -0600

    Begin working on personalization

--- src/components/atoms/LogoIcon.js
+++ src/components/atoms/LogoIcon.js
@@ -1,17 +1,17 @@
-import IconButton from "./IconButton"
+import IconButton from "./IconButton";
 
-let logo = {
-  viewBox: "0 0 61 32",
-  href: '/',
-  path: (
-    <path fill="currentColor" fillRule="evenodd" d="M60.082 5.878L44.408 32 28.735 5.878h31.347zM15.673 0l15.674 26.122H0L15.673 0z"/>
-  )
-}
+import logo from "../../images/icons/logo";
+
+// let logo = {
+//   viewBox: "0 0 61 32",
+//   href: '/',
+//   path: (
+//     <path fill="currentColor" fillRule="evenodd" d="M60.082 5.878L44.408 32 28.735 5.878h31.347zM15.673 0l15.674 26.122H0L15.673 0z"/>
+//   )
+// }
 
 const LogoIcon = (props) => {
-  return (
-    <IconButton {...props} iconObject={logo} />
-  )
-}
+  return <IconButton {...props} iconObject={logo} />;
+};
 
 export default LogoIcon;
